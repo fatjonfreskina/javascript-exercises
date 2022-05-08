f7e1f32935741c468e5d43c997d34648fb863f1a
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-05-08 23:00:51 +0200

    Solved excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+ const reverseString = function reverseString(input) {
+	var i = input.length - 1;
+	var reversed = '';
+	while (i > -1){
+		reversed += input[i];
+		i -= 1;
+	}
+	return reversed
 };
 
 // Do not edit below this line
