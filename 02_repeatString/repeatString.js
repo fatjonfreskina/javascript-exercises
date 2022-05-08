dc5980deea1a2f8535839dc717cc134b0bcc3067
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-05-08 22:42:49 +0200

    solved challenge 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+function repeatString(string, num){
 
+	if (num < 0) {return 'ERROR'}; //handle exception
+
+	var i = 0 ;
+	var repeatedString = '';
+
+	while (i<num)
+	{
+		repeatedString += string;
+		i++;
+	}
+
+	return repeatedString;
 };
 
 // Do not edit below this line
