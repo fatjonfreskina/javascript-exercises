4e6de066560d7f04ca3e329e6b2498ef657ff0ae
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-05-10 00:08:31 +0200

    solved excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+	for  (var j = 1; j < arguments.length; j++){
 
+		for (var i = 0; i < arr.length; i++){ 
+	    
+	        if (arr[i] === arguments[j]) { 
+	    
+	            arr.splice(i, 1); 
+	        }
+	    }
+    }
+    return arr;
 };
 
 // Do not edit below this line
