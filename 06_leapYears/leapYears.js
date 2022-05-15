6ab0198642b94f15105a0d6178790e15bef12f97
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-05-15 22:04:36 +0200

    Solved challenges 6 and 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	if (year%4 == 0){
+		if (year%100 == 0){
+			if (year%400 == 0){ 
+				return true; // divisible by 4, by 100 and by 400!
+			} else {
+				return false; // divisible by 4, by 100, not by 400 :(
+			}
+		} else {
+			return true; // divisible by for, but not by 100
+		}
+	} else {
+		return false; // not divisible by 4
+	}
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fahreneit) {
+//ftoc(32) // fahrenheit to celsius, should return 0
+var toCelsius = (fahreneit - 32) * 5/9;
+var rounded = Math.round(toCelsius * 10) / 10;
+return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+//ctof(0) // celsius to fahrenheit, should return 32
+var toFahreneit = celsius*9/5 + 32;
+var rounded = Math.round(toFahreneit * 10) / 10;
+return rounded
 };
 
 // Do not edit below this line
