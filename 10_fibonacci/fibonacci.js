b044f592ab9565d85073231897eaeed7879862d3
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-05-17 23:04:30 +0200

    Solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+	var input = parseInt(number);
+	if (number < 1) { return "OOPS"}
 
+	fib = [1,1,2,3,5];
+	if (input <= fib.length) {
+		return fib[input-1];
+	} else {
+		for (var i = 4; i < input; i++){
+			new_fib = fib [i] + fib[i-1];
+			fib.push(new_fib);
+		}
+	}
+	return fib[input-1];
 };
 
 // Do not edit below this line
