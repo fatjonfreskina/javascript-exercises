adbafb012e9e2918b99d9a37a212fa74965298a0
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-05-17 22:47:20 +0200

    Solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+	var word = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	word = word.toLowerCase().trim().replace(/\s/g, '');
+	console.log(word);
+	last = word.length - 1;
+	for (var i = 0; i < word.length; i++){
+		console.log("word[i] = " + word[i] + "; word[last] = " + word[last]);
+		if (word[i] != word[last]) {
+		 return false;
+		}
+		last -= 1;
+	}
+	return true;
 };
 
 // Do not edit below this line
