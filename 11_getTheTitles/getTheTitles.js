3d106be18c3f3884b764d6d99647112d60ba0a87
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-05-18 23:46:17 +0200

    Solved challenge 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+	var titles = [];
+	for (var i = 0 ; i < arr.length; i++){
+		/*var title = arr[i]["title"];*/
+		// titles.push(title);
+	}
+	// Or, using the built in forEach function
+	arr.forEach(obj => titles.push(obj["title"]));
+	return titles;
 };
 
 // Do not edit below this line
